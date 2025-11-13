2e8c95d2ad58e42df1a23bb1dd88e1858cc286e7
wasahara <wasahara@student.unimelb.edu.au>
2025-11-14 01:56:02 +1100

    removed unnecessary tftprint

--- src/App.h
+++ src/App.h
@@ -19,45 +19,21 @@ bool touchLeft(XPT2046_Touchscreen &ts, int threshold = 1900);
 bool delayMillis(unsigned long lastTime, unsigned long interval);
 
 // TFT print helpers that behave like Serial.print / Serial.println
-// void tftPrint(TFT_eSPI &tft, const String &s);
+void tftPrint(TFT_eSPI &tft, const String &s);
 void tftPrint(TFT_eSPI &tft, const char *s);
 void tftPrint(TFT_eSPI &tft, char c);
-// void tftPrint(TFT_eSPI &tft, int v);
-// void tftPrint(TFT_eSPI &tft, unsigned int v);
-// void tftPrint(TFT_eSPI &tft, long v);
-// void tftPrint(TFT_eSPI &tft, unsigned long v);
-// void tftPrint(TFT_eSPI &tft, float v, int decimals = 2);
-// void tftPrint(TFT_eSPI &tft, double v, int decimals = 2);
 
 // void tftPrintln(TFT_eSPI &tft);
 // void tftPrintln(TFT_eSPI &tft, const String &s);
 void tftPrintln(TFT_eSPI &tft, const char *s);
 void tftPrintln(TFT_eSPI &tft, char c);
-// void tftPrintln(TFT_eSPI &tft, int v);
-// void tftPrintln(TFT_eSPI &tft, unsigned int v);
-// void tftPrintln(TFT_eSPI &tft, long v);
-// void tftPrintln(TFT_eSPI &tft, unsigned long v);
-// void tftPrintln(TFT_eSPI &tft, float v, int decimals = 2);
-// void tftPrintln(TFT_eSPI &tft, double v, int decimals = 2);
 
 // --- New: Serial-like wrappers that use the global 'tft' ---
 // void tftPrint(const String &s);
 void tftPrint(const char *s);
 void tftPrint(char c);
-// void tftPrint(int v);
-// void tftPrint(unsigned int v);
-// void tftPrint(long v);
-// void tftPrint(unsigned long v);
-// void tftPrint(float v, int decimals = 2);
-// void tftPrint(double v, int decimals = 2);
 
 // void tftPrintln();
 // void tftPrintln(const String &s);
 void tftPrintln(const char *s);
 void tftPrintln(char c);
-// void tftPrintln(int v);
-// void tftPrintln(unsigned int v);
-// void tftPrintln(long v);
-// void tftPrintln(unsigned long v);
-// void tftPrintln(float v, int decimals = 2);
-// void tftPrintln(double v, int decimals = 2);
